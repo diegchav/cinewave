2104a14c4500da3cd1e339c178cc05c6cfba253c
Diego Chávez <diego.ch.schez@gmail.com>
2025-11-03 13:29:11 -0600

    feat: add cinewave-movie-service module

---
+++ cinewave-movie-service/src/main/java/com/diegchav/cinewave/movie/CinewaveMovieServiceApplication.java
@@ -0,0 +1,11 @@
+package com.diegchav.cinewave.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinewaveMovieServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CinewaveMovieServiceApplication.class, args);
+	}
+}
---
+++ cinewave-movie-service/src/test/java/com/diegchav/cinewave/movie/CinewaveMovieServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.diegchav.cinewave.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinewaveMovieServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
